94de805eb787a0d7d2a82699bbaa8957839b3a13
Idris-it <juraev.idris@mail.ru>
2025-12-23 12:13:47 +0300

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -151,7 +151,7 @@ def handle_keys(snake: Snake):
 def main():
     """Основной игровой цикл."""
     global screen, clock
-    
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption('Изгиб Питона')
